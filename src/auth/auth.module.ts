ab043134189c3a26ab3ecce95db8ae9c389005ae
Eduardo Iotti <ddz.iotti@gmail.com>
2024-01-27 14:19:28 -0300

    Modify expiry date

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,7 +17,7 @@ import { APP_GUARD } from '@nestjs/core';
       global: true,
       secret: process.env.JWT_SECRET,
 
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '1d' },
     }),
   ],
   controllers: [AuthController],
